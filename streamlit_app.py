dff3b0ade0785acf6566425f4cf6a8400ddf9120
avalanej <ali.valanejad@yahoo.fr>
2023-01-31 14:56:33 +0100

    Update streamlit_app.py
    breakfast menu and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
-streamlit.title ('My parents new healthy diner!');
-streamlit.header ('Breakfast Menu');
-streamlit.header ('Cheese & Bread!');
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
