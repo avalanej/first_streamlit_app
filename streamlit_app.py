47137636aad76dd5361f7fc8097c25418db7dd5d
avalanej <ali.valanejad@yahoo.fr>
2023-02-05 13:08:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,15 +16,19 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+def get_fruityvice_data (this_fruit_choice)
+     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+     return fruityvice_normalized
 streamlit.dataframe(fruits_to_show)
 try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice:
       streamlit.error('please select a fruit to get information')
    else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized)
+      back_from_function = get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
 except URLError as e:
    streamlit.error()
 
@@ -49,4 +53,6 @@ streamlit.dataframe(my_data_rows)
 fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Tanks for adding ', fruit_choice)
 my_cur.execute("insert into  fruit_load_list values ('from streamlit')")
+
+
 
